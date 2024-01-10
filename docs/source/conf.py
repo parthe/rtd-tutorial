08b19480d34524028544e60c65e323871c97b11b
Parthe Pandit <parthe@users.noreply.github.com>
2024-01-11 01:44:08 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'EigenPro'
+copyright = '2023, Belkin lab'
+author = 'Amirhesam Abedsoltan, Siyuan Ma, Parthe Pandit'
 
 release = '0.1'
 version = '0.1.0'
